e3dacb25941b4ccec7b65aaa5725207513092b52
Siddhesh1420 <siddheshdiptil@gmail.com>
2025-07-15 15:08:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-const indexRouter = require('./routes/index');
+const indexRouter = require('./index');
 
 app.use(express.urlencoded({ extended: true })); // To parse form data
 app.use('/', indexRouter);
